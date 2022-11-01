8eed92d588bbc2d521fde5f99567fea7be58ce83
iqbalalmaududi <115415097+iqbalalmaududi@users.noreply.github.com>
2022-11-01 19:20:27 +0700

    Add files via upload

---
+++ INHERITANCE_EMAIL.py
@@ -0,0 +1,14 @@
+class pegawai:
+    def __init__(self, nama,):
+        self.nama=nama
+        self.email=  nama+ "@gmail.com"
+
+class company(pegawai):
+    pass
+       
+
+    
+    
+pegawai1= pegawai("iqbalalmaudi")
+print(pegawai1.email)
+
