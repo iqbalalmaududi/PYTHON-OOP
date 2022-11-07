a0015806a5a7ae9adde9c43997edec175e4c922c
iqbalalmaududi <115415097+iqbalalmaududi@users.noreply.github.com>
2022-11-07 17:57:45 +0700

    Add files via upload

---
+++ special method_dunder basic.py
@@ -0,0 +1,17 @@
+class String:
+      
+    # magic method to initiate object
+    def __init__(self, string):
+        self.string = string
+    
+    def __repr__(self):
+        return 'Object: {}'.format(self.string)
+          
+# Driver Code
+if __name__ == '__main__':
+      
+    # object creation
+    string1 = String('Hello')
+  
+    # print object location
+    print(string1)
