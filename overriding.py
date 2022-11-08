b257ecf0c60f02d74e67c949f706d6c175b4d8b7
iqbalalmaududi <115415097+iqbalalmaududi@users.noreply.github.com>
2022-11-08 19:47:07 +0700

    Add files via upload

---
+++ overriding.py
@@ -0,0 +1,10 @@
+class mamalia:
+    def ciri(self):
+        print ("menyusui dan melahirkan")
+
+class paus(mamalia):
+    def ciri(self):
+        print  ("menyusui, melahirkan dan di laut")
+
+animal1 = paus()
+animal1.ciri()
